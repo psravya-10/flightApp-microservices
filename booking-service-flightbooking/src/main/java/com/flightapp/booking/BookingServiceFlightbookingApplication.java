6e35e3347dda5a579966563e8e6bbbcc6bbce9e0
sravya <podilasravya7052@gmail.com>
2025-12-06 00:11:21 +0530

    Update application main classes

--- booking-service-flightbooking/src/main/java/com/flightapp/booking/BookingServiceFlightbookingApplication.java
+++ booking-service-flightbooking/src/main/java/com/flightapp/booking/BookingServiceFlightbookingApplication.java
@@ -14,5 +14,6 @@ public class BookingServiceFlightbookingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceFlightbookingApplication.class, args);
 	}
+	 
 
 }
